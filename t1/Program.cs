8929f64bbc8c3be42194a0cc74e42302adb987bb
Denis <asmusdish@ya.ru>
2023-02-06 18:20:21 +0500

    add tasks

---
+++ t1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    return array;
+}
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int value;
+        while (!int.TryParse(Console.ReadLine(), out value))
+        {
+            Console.WriteLine("Неверное значение");             
+        }  
+        array[i] = value;
+    }
+    return array;
+}
+
+int FindPozitive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;        
+    }
+    return count;    
+}
+
+Console.WriteLine("Кол-во значений: ");
+int size;
+while (!int.TryParse(Console.ReadLine(), out size) || size == 0)
+{
+    Console.WriteLine("Неверное значение");            
+}
+Console.WriteLine("Введите значения через enter:");
+
+Console.WriteLine($"Кол-во значений больше нуля: {FindPozitive(FillArray(Array(size)))}");
---
+++ t2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+(float, float, bool) FindCrossPoint (float b1, float k1, float b2, float k2) {
+   float x = 0;
+   float y = 0;
+   bool parallel = false;
+   if (!(k1 == k2))
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;
+    }
+    else
+    {
+        Console.WriteLine($"Линии параллельны");
+        parallel = true;
+    }
+    return (x, y, parallel);
+}
+
+Console.WriteLine("Введите b1");
+float b1;
+while (!float.TryParse(Console.ReadLine(), out b1))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+Console.WriteLine("Введите k1");
+float k1;
+while (!float.TryParse(Console.ReadLine(), out k1))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+Console.WriteLine("Введите b2");
+float b2;
+while (!float.TryParse(Console.ReadLine(), out b2))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+Console.WriteLine("Введите k2");
+float k2;
+while (!float.TryParse(Console.ReadLine(), out k2))
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+
+(float x, float y, bool parallel) = FindCrossPoint(b1, k1, b2, k2);
+
+if (!parallel) 
+Console.WriteLine($"Точка пересечения: ({x}, {y})");
+
+
